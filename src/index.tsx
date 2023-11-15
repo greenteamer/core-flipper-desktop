f4c6e4dc2b7b7ac47dcfbaf645b26a9d2b26ad33
Aleksander Korovkin <teamer777@gmail.com>
2023-11-15 20:51:10 +0400

    add app state discovering

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import { Typography } from "antd";
+import { Button, Table, Typography } from "antd";
 import {
   DataInspector,
-  DataTableColumn,
   DetailSidebar,
   Layout,
   PluginClient,
   createState,
-  createTablePlugin,
   usePlugin,
-  // create
+  useValue,
 } from "flipper-plugin";
 import React from "react";
 
@@ -18,29 +16,155 @@ type Row = {
   step: string;
 };
 
-const columns: DataTableColumn<Row>[] = [
-  {
-    key: "action",
-    width: 150,
-    title: "Action",
-  },
-  {
-    key: "step",
-    title: "Step",
-  },
-];
+type Events = {
+  appState: any;
+  newRow: Row;
+  reset: any;
+};
 
-const { plugin, Component } = createTablePlugin<Row>({
-  columns,
-  method: "newRow",
-  key: "id",
-  resetMethod: "reset",
-  renderSidebar: (row) => (
-    <div>
-      <span>{row.action}</span> - <span>{row.step}</span>
+type Methods = {
+  calloutStepError(): Promise<{ message: string }>;
+};
+
+export function plugin(client: PluginClient<Events, Methods>) {
+  const appState = createState<Record<string, any>>(
+    {},
+    { persist: "appState" }
+  );
+
+  const appStateDisplayed = createState<boolean>(true, {
+    persist: "appStateDisplayed",
+  });
+
+  const rows = createState<Record<string, Row>>({}, { persist: "rows" });
+  const selectedID = createState<string | null>(null, { persist: "selection" });
+
+  client.onMessage("appState", (state) => {
+    appState.set(state);
+  });
+
+  client.onMessage("newRow", (row) => {
+    rows.update((draft) => {
+      draft[row.id] = row;
+    });
+  });
+
+  client.onMessage("reset", () => {
+    rows.set({});
+  });
+
+  client.addMenuEntry({
+    label: "Reset Selection",
+    accelerator: "CmdOrCtrl+R",
+    handler: () => {
+      rows.set({});
+    },
+  });
+
+  function setSelection(id: number) {
+    selectedID.set("" + id);
+  }
+
+  function showAppState() {
+    appStateDisplayed.set(!appStateDisplayed.get());
+  }
+
+  function setError() {
+    client.send("calloutStepError", undefined);
+  }
+
+  return {
+    appState,
+    appStateDisplayed,
+    showAppState,
+    rows,
+    selectedID,
+    setSelection,
+    setError,
+  };
+}
+
+export function Component() {
+  const instance = usePlugin(plugin);
+  const appState = useValue(instance.appState);
+  const appStateDisplayed = useValue(instance.appStateDisplayed);
+  const rows = useValue(instance.rows);
+  const selectedID = useValue(instance.selectedID);
+
+  const dataSource = Object.entries(rows).map((i) => i[1]);
+  console.log(appStateDisplayed);
+
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+    },
+    {
+      title: "Step",
+      dataIndex: "step",
+      key: "step",
+    },
+  ];
+
+  // (4)
+  return (
+    <>
+      <Button onClick={instance.setError}>Test Error</Button>
+      <Button onClick={instance.showAppState}>
+        {appStateDisplayed ? "Hide" : "Show"} App State
+      </Button>
+      <Layout.ScrollContainer vertical>
+        <Layout.Horizontal gap pad style={{ flexWrap: "wrap" }}>
+          <Table
+            dataSource={dataSource}
+            columns={columns}
+            onRow={(record, rowIndex) => {
+              return {
+                onClick: () => {
+                  instance.setSelection(record.id);
+                },
+              };
+            }}
+            rowSelection={{
+              onSelect: (record) => instance.setSelection(record.id),
+              selectedRowKeys: selectedID ? [selectedID] : [],
+            }}
+          />
+        </Layout.Horizontal>
+      </Layout.ScrollContainer>
+      <DetailSidebar>
+        {appStateDisplayed
+          ? renderAppStateSidebar(appState)
+          : selectedID
+          ? renderSidebar(rows[selectedID])
+          : null}
+      </DetailSidebar>
+    </>
+  );
+}
+
+function renderAppStateSidebar(appState: any) {
+  return (
+    <Layout.Container gap pad>
+      <Typography.Title level={4}>App State</Typography.Title>
+      <DataInspector data={appState} expandRoot={true} />
+    </Layout.Container>
+  );
+}
+
+function renderSidebar(row: Row) {
+  return (
+    <Layout.Container gap pad>
+      <Typography.Title
+        level={4}
+      >{`${row.id}: ${row.action} - ${row.step}`}</Typography.Title>
       <DataInspector data={row} expandRoot={true} />
-    </div>
-  ),
-});
-
-export { plugin, Component };
+    </Layout.Container>
+  );
+}
